6cbae830569560f05394948e31a64564255ce721
Kayla <knchavez14@gmail.com>
2021-10-26 17:13:24 -0700

    need to validate data

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,7 +1,7 @@
 const service = require("./reservations.service");
 const hasProperties = require("../errors/hasProperties")
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
-const { as } = require("../db/connection");
+//const { as } = require("../db/connection");
 /**
  * List handler for reservation resources
  */
@@ -17,15 +17,18 @@ if(reservation){
 next({ status: 404, message: `Reservation ${reservation_id} cannot be found.`});
 }
 
-//return date and time
- async function list(req, res) {
-  const data = await service.list();
-  res.json({ data });
+//List Handler for Date
+async function list(req, res) {
+  const { date } = req.query;
+  // console.log("***",date);
+  if (date) {
+    return res.json({ data: await service.list(date) });
+  }
 }
 
- async function listByDate(req,res){
-   res.json({ data: await service.listByDate() });
- }
+//  async function listByDate(req,res){
+//     res.json({ data: await service.listByDate(date) });
+//   }
 //  async function listByPhone(req,res){
 //    res.json({ data: await service.listByPhone() });
 //  }
@@ -65,12 +68,56 @@ function hasOnlyValidProperties(req, res, next) {
 }
 const hasRequiredProperties = hasProperties(...VALID_PROPERTIES);
 
+
+
+//VALIDATE IF PEOPLE IS A NUMBER
+function validatePeople(req, res, next){
+  const {people} = req.body.data;
+  if(typeof people !== "number"){
+    return next({
+      status: 400,
+      message: `people should be a number`
+    });
+  };
+  next();
+}
+//VALIDATE RESERVATION DATE AND TIME FORMAT
+
+function validateDate(req, res, next) {
+  const date = req.body.data.reservation_date;
+  const valid = Date.parse(date);
+
+  if (valid) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "reservation_date must be valid date.",
+  })
+}
+
+function validateTime(req, res, next){
+  const regex = "([01]?[0-9]|2[0-3]):[0-5][0-9]";
+  const time = req.body.data.reservation_time
+  const timeValid = time.match(regex)
+
+  if(timeValid){
+    return next();
+  }
+  next({
+    status: 400,
+    message: "reservation_time must be valid time.",
+  })
+}
+
 module.exports = {
-  list,
-  listByDate,
+  list: asyncErrorBoundary(list),
   create:[
     asyncErrorBoundary(hasRequiredProperties),
     asyncErrorBoundary(hasOnlyValidProperties),
+    asyncErrorBoundary(validatePeople),
+    asyncErrorBoundary(validateDate),
+    asyncErrorBoundary(validateTime),
     asyncErrorBoundary(create)],
-  read:[reservationExists, read],
+  read:[asyncErrorBoundary(reservationExists), read],
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,18 +1,21 @@
 const knex = require("../db/connection");
-//
-function list() {
-  return knex("reservations")
-  .select("*");
+
+function list(date) {
+   const query = knex("reservations")
+      .select("*")
+      .where({ reservation_date: date })
+      .orderBy("reservation_time", "asc")
+      return query
 }
 
-function listByDate(reservation_date){
-  return knex("reservations")
-  .select("*")
-  .where({reservation_date})
-  .whereNot("status","finished")
-  .andWhereNot("status","cancelled")
-  .orderBy("reservation_time")
-}
+// function listByDate(reservation_date){
+//   return knex("reservations")
+//   .select("*")
+//   .where({reservation_date})
+//   .whereNot("status","finished")
+//   .andWhereNot("status","cancelled")
+//   .orderBy("reservation_time");
+// }
 
 
 
@@ -32,7 +35,6 @@ function read(reservation_id){
 
 module.exports = {
   list,
-  listByDate,
   create,
   read,
 };
